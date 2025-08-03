bf938a68dea16cdc805658871b68764db138ae28
sspartorg <sspart.org@gmail.com>
2025-08-04 00:07:05 +0530

    Initial Commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-  return <h1>Welcome to Vite React Website!</h1>;
+  return <h1>Welcome to Vite React hi Website!</h1>;
 };
 
 export default Home;
